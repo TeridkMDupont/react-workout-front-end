3329cbe8dbb1586b8e549548d2c6d5252670e4a9
TerickD <TerickMDupont@gmail.com>
2025-11-12 18:14:06 -0500

    punctuation update

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <img src="https://i.imgur.com/3rbIMus.png" alt="figure lifting weights" className={styles.heroImage}/>
       </div>
       <section className={styles.textSection}>
-      <h1 className={styles.title}>Welcome to Community Gains!An application built to bring people together and improve their lives at the same time!</h1>
+      <h1 className={styles.title}>Welcome to Community Gains! An application built to bring people together and improve their lives at the same time!</h1>
       <p className={styles.subtitle}>Sign up or sign in now to partake in the gains!!</p>
       </section>
     </main>
